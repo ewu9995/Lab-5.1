6a2fbc7d476b4dcedf5bca6c18920d36174b66e1
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-05-31 09:10:08 -0400

    L

---
+++ BreadthFirstSearch/src/Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+
+	public String cityName;
+	Node leftChild;
+	Node rightChild;
+	
+	public Node(String cityName, Node firstChild, Node secondChild)
+	{
+		this.cityName = cityName;
+	}
+	
+}
